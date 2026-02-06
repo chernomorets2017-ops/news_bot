44dcbc29b5dbac7d0b58085f033e592b2b25dc0f
chernomorets2017-ops <chernomorets2017@gmail.com>
2026-02-06 19:54:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,24 @@
+import os
+
 BOT_TOKEN = "8546746980:AAF3z5K85WaBMC-SKTSTN5Tx_dXxXyZXIoQ"
 CHANNEL_USERNAME = "SUP_V_BOTK"
-CHANNEL_LINK = "https://t.me/SUP_V_BotK"
-LANGUAGE = "russian"
+CHANNEL_LINK = "https://t.me/SUP_V_BOTK"
+
+RSS_FEEDS = [
+    "https://lenta.ru/rss/news",
+    "https://www.intermedia.ru/rss",
+    "https://www.the-flow.ru/rss/all",
+    "https://www.billboard.com/feed/",
+    "https://pitchfork.com/rss/news/"
+]
+
+KEYWORDS = [
+    "music", "album", "track", "single",
+    "rapper", "singer", "band", "concert",
+    "музыка", "артист", "альбом", "клип",
+    "шоу-бизнес", "рэпер", "певец"
+]
+
+POSTS_PER_RUN = 1
+
+FALLBACK_IMAGE = "https://i.imgur.com/8Km9tLL.jpg" 

94e2b8435179ab17cf8738e670275512c0f1de0c
chernomorets2017-ops <chernomorets2017@gmail.com>
2026-02-07 09:33:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,52 @@
 import os
 import json
 import feedparser
+import random
 from telegram import Bot
 
+# ===== СЕКРЕТЫ =====
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
-CHANNEL_LINK = os.getenv("CHANNEL_LINK")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")  # @yourchannel
+CHANNEL_LINK = os.getenv("CHANNEL_LINK")          # https://t.me/yourchannel
 
+# ===== НАСТРОЙКИ =====
 RSS_FEEDS = [
-    "https://news.google.com/rss/search?q=music+celebrity+news",
+    "https://news.google.com/rss/search?q=music+celebrity+news&hl=en&gl=US&ceid=US:en",
+    "https://news.google.com/rss/search?q=show+business+news&hl=en&gl=US&ceid=US:en"
 ]
 
-KEYWORDS = ["music", "album", "song", "singer", "rapper", "band"]
+KEYWORDS = ["music", "album", "song", "singer", "rapper", "band", "celebrity", "concert"]
 POSTS_PER_RUN = 2
-FALLBACK_IMAGE = "https://i.imgur.com/8Km9tLL.jpg"
+
+IMAGES = [
+    "https://i.imgur.com/8Km9tLL.jpg",
+    "https://i.imgur.com/Z6X9Z9s.jpg",
+    "https://i.imgur.com/1c9a1aF.jpg"
+]
 
 bot = Bot(BOT_TOKEN)
 
 
+# ===== АНТИ-ДУБЛИКАТЫ =====
 def load_posted():
     try:
         return set(json.load(open("posted.json")))
     except:
         return set()
 
+def save_posted(data):
+    json.dump(list(data), open("posted.json", "w"))
 
-def save_posted(urls):
-    json.dump(list(urls), open("posted.json", "w"))
 
-
+# ===== ОСНОВНАЯ ЛОГИКА =====
 def run():
     posted = load_posted()
     sent = 0
 
-    for feed in RSS_FEEDS:
-        data = feedparser.parse(feed)
+    for feed_url in RSS_FEEDS:
+        feed = feedparser.parse(feed_url)
 
-        for e in data.entries:
+        for e in feed.entries:
             if sent >= POSTS_PER_RUN:
                 break
 
@@ -44,14 +54,35 @@ def run():
             link = e.link
             summary = e.summary if "summary" in e else ""
 
-            if any(k in title.lower() for k in KEYWORDS) is False:
+            text_lower = (title + summary).lower()
+
+            # фильтр по теме
+            if not any(k in text_lower for k in KEYWORDS):
                 continue
+
+            # анти-дубликат
             if link in posted:
                 continue
 
-            text = f"<b>{title}</b>\n\n{summary[:400]}...\n\n<a href='{CHANNEL_LINK}'>Channel</a>"
+            # короткий пересказ
+            summary = summary.replace("<b>", "").replace("</b>", "")
+            summary = summary.replace("<a", "").replace("</a>", "")
+            summary = summary[:450]
 
-            bot.send_photo(chat_id=CHANNEL_USERNAME, photo=FALLBACK_IMAGE, caption=text, parse_mode="HTML")
+            text = f"""<b>{title}</b>
+
+{summary}...
+
+<a href="{CHANNEL_LINK}">Подписаться на канал</a>"""
+
+            image = random.choice(IMAGES)
+
+            bot.send_photo(
+                chat_id=CHANNEL_USERNAME,
+                photo=image,
+                caption=text,
+                parse_mode="HTML"
+            )
 
             posted.add(link)
             sent += 1

31c95b0a2c55696de34929ed6f18d90b9d7ea3c6
chernomorets2017-ops <chernomorets2017@gmail.com>
2026-02-13 05:36:06 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,17 +1,30 @@
 import feedparser
 
 FEEDS = [
-    "https://rss.app/feeds/_GkzYwQk9kK7rYj9m.xml",  # пример, можешь заменить
+   
     "https://rss.cnn.com/rss/edition.rss",
     "https://www.theguardian.com/world/rss",
+
+   
+    "https://lenta.ru/rss/news",
+    "https://www.rbc.ru/rss/news"
 ]
 
 def get_news():
-    news = []
+    result = []
     for url in FEEDS:
         feed = feedparser.parse(url)
-        for entry in feed.entries[:5]:
-            title = entry.title
-            link = entry.link
-            news.append(f"{title}\n{link}")
-    return news
+        for e in feed.entries[:7]:
+            title = e.title
+            link = e.link
+            summary = getattr(e, "summary", "")
+
+            if len(title) < 15:
+                continue
+
+            result.append({
+                "title": title,
+                "link": link,
+                "summary": summary
+            })
+    return result

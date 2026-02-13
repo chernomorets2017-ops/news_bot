c661c5cb16bdb20da691225a76a06963d910238a
chernomorets2017-ops <chernomorets2017@gmail.com>
2026-02-13 16:08:02 +0300

    Delete translator.py

--- translator.py
+++
@@ -1,9 +0,0 @@
-from googletrans import Translator
-
-translator = Translator()
-
-def translate(text):
-    try:
-        return translator.translate(text, dest="ru").text
-    except:
-        return text

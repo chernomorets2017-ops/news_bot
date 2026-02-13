dbab370b37983c27869cf7b1cbc4af5cbf4e7ee3
chernomorets2017-ops <chernomorets2017@gmail.com>
2026-02-13 16:05:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,53 +1,36 @@
-import json
-import hashlib
-import time
 from telegram import Bot
-from config import BOT_TOKEN, CHANNEL, SIGN_LINK
 from parser import get_news
-from translator import translate
+import json
+import time
+from config import BOT_TOKEN, CHANNEL, SIGN
 
 bot = Bot(BOT_TOKEN)
 
-def load_db():
+def load_posted():
     try:
-        return set(json.load(open("db.json")))
+        with open("posted.json") as f:
+            return json.load(f)
     except:
-        return set()
+        return []
 
-def save_db(db):
-    json.dump(list(db), open("db.json", "w"))
+def save_posted(data):
+    with open("posted.json", "w") as f:
+        json.dump(data, f)
 
-def hash_news(t):
-    return hashlib.md5(t.encode()).hexdigest()
+posted = load_posted()
 
-def main():
-    db = load_db()
+while True:
     news = get_news()
-
     for n in news:
-        key = hash_news(n["title"])
-        if key in db:
+        if n["title"] in posted:
             continue
 
-        title = translate(n["title"])
-        text = translate(n["text"])[:1000]
+        text = f"🌍 Мир\n\n{n['title']}\n\n{supnews}\n{SIGN}"
 
-        caption = f"""🌍 <b>{title}</b>
+        bot.send_message(CHANNEL, text)
+        posted.append(n["title"])
+        save_posted(posted)
 
-{text}
+        time.sleep(10)
 
-<a href="{SIGN_LINK}">.sup.news</a>
-"""
-
-        if n["img"]:
-            bot.send_photo(CHANNEL, n["img"], caption=caption, parse_mode="HTML")
-        else:
-            bot.send_message(CHANNEL, caption, parse_mode="HTML")
-
-        db.add(key)
-        time.sleep(5)
-
-    save_db(db)
-
-if __name__ == "__main__":
-    main()
+    time.sleep(3600)
